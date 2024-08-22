b7666ecc929036a6c8c088a428a96a1305a3b695
MarAntBQ <github@marantbq.dev>
2024-08-21 23:11:36 -0500

    chore: Update npm dependencies and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteSass from 'vite-plugin-sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSass()],
 })
