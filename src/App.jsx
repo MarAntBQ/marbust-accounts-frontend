c5261b783cd3fc5be00ef5cd8dd6835d0bc5887c
MarAntBQ <github@marantbq.dev>
2024-08-21 23:33:48 -0500

    feat: Agregar componente HeaderComponent a la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { HeaderComponent } from './layout/HeaderComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <HeaderComponent />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank">Vite Logo
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://react.dev" target="_blank">React Logo</a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
---
+++ src/layout/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><a href='./'>Inicio</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
