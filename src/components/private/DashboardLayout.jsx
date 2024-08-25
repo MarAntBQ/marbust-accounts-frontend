16f7c5f2d685f6ed08ffce6a7440c48344e6a4d8
MarAntBQ <github@marantbq.dev>
2024-08-24 22:59:52 -0500

    chore: Remove unused components, update file structure, and improve user profile menu

--- src/components/private/DashboardLayout.jsx
+++ src/components/private/DashboardLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Header } from './layout/Header'
 import { Outlet } from 'react-router-dom'
-import { Home } from './pages/Home'
 
 export const DashboardLayout = ({ setToken, token }) => {
   return (
@@ -9,10 +8,6 @@ export const DashboardLayout = ({ setToken, token }) => {
       <Header setToken={setToken}/>
       <main>
       <Outlet/>
-      {/* <Routes>
-        <Route path='/dashboard' element={<Home/>}/>
-        <Route path="*" element={<Home/>}></Route>
-      </Routes> */}
       </main>
     </div>
   )
--- src/components/private/layout/User.jsx
+++ src/components/private/layout/User.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom'
 
 export const User = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("toggleMenu");
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-    <div>
-        IMG(User)
-        <a>Mi Perfil</a>
-        <a>Editar Password</a>
-        <a>Logout</a>
+    <div className='header-profile'>
+        <div className="header-profile__section" onClick={toggleMenu}>
+          <img src='/img/public/header/profile-ico.png' alt='Default User Icon'/>
+          <p>MarAntBQasddddassasadadssdaadaadsdasdsaasdaadsasdasdads</p>
+        </div>
+        <ul className={`header-profile__options ${isMenuOpen ? 'header-profile__options--open' : ''}`}>
+          <li><NavLink to="/dashboard/my-account">Mi Perfil</NavLink></li>
+          <li><NavLink to="/dashboard/my-account/change-password">Editar Password</NavLink></li>
+          <li><a>Logout</a></li>
+        </ul>
     </div>
   )
 }
