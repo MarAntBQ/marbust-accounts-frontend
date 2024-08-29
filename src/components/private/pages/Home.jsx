4d5b72dc61c2e4adf797863a4b175b7c1d676e9b
MarAntBQ <github@marantbq.dev>
2024-08-29 00:09:58 -0500

    feat: Add user authentication to Home component

--- src/components/private/pages/Home.jsx
+++ src/components/private/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import useAuth from '../../../hooks/useAuth'
 
 export const Home = () => {
+  const {auth} = useAuth();
   return (
     <div>
       <h1>Welcome to  Marbust Accounts&reg; 2.5.0</h1>
+      <hr />
+      {auth && auth.firstName && auth.lastName && (<h2>{auth.firstName + ' ' + auth.lastName}</h2>)}
         <br />
         <ol>
           <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
