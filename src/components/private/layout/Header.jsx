7f5ec14a132196d4aae30d4ba830a6faa23a1e1c
MarAntBQ <github@marantbq.dev>
2024-08-29 02:10:48 -0500

    chore: Update Header component to add admin navigation options and handle user authentication

--- src/components/private/layout/Header.jsx
+++ src/components/private/layout/Header.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { User } from './User';
+import useAuth from '../../../hooks/useAuth';
 
 export const Header = () => {
+  const {auth} = useAuth();
   return (
     <header>
       <User/>
         <nav>
             <ul>
                 <li><NavLink to="/dashboard">Inicio</NavLink></li>
-                <li><a href='./'>Administración del Sistema</a>
+                {auth.roleId >= 3 && (
+                  <>
+                  <hr/>
+                  <li><a href='./'>Administración del Sistema</a>
                   <ul>
                     <li><a href='./'>Usuarios</a></li>
                     <li><a href='./'>Roles</a></li>
                     <li><a href='./'>Permisos</a></li>
                   </ul>
                 </li>
+              <hr/>
+              </>)}
+                <li><NavLink to="./">Marbust Computers</NavLink></li>
+                <li><NavLink to="./">MBHostCloud</NavLink></li>
+                <li><NavLink to="./">Marbust Sites</NavLink></li>
             </ul>
         </nav>
     </header>
--- src/components/private/pages/Home.jsx
+++ src/components/private/pages/Home.jsx
@@ -1,63 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import useAuth from '../../../hooks/useAuth'
+import Global from '../../../helpers/Global';
+import axios from 'axios';
 
 export const Home = () => {
   const {auth} = useAuth();
+
+  const [updates, setUpdates] = useState([]);
+
+  useEffect(() => {
+    const fetchUpdates = async () => {
+      try {
+        const response = await axios.get(`${Global.url}/system-updates/view-all`, {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        setUpdates(response.data);
+      } catch (error) {
+        console.error('Error fetching updates:', error);
+      }
+    };
+
+    fetchUpdates();
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to  Marbust Accounts&reg; 2.5.0</h1>
+    <div className='dashboard__home'>
+      <h1>Bienvenido/a  a Marbust Accounts&reg; 2.5.0</h1>
       <hr />
-      {auth && auth.firstName && auth.lastName && (<h2>{auth.firstName + ' ' + auth.lastName}</h2>)}
-        <br />
-        <ol>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-          <li><strong>Update (Aug 22, 2024):</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus, maxime officia, reiciendis porro quis blanditiis fugit assumenda cum ut distinctio tempora pariatur cupiditate. Nihil dolorum deserunt reiciendis quasi aut.</li>
-        </ol>
-    </div>
+      {auth && auth.firstName && auth.lastName && (<h5>{auth.firstName + ' ' + auth.lastName}</h5>)}
+        <hr/>
+        <h2>Actualizaciones del sistema</h2>
+        <ol className='update-list'>
+        {updates.map(update => (
+          <li key={update.id} className="update-item">
+          <div className="update-title">{update.title}:</div>
+          <div className="update-description">{update.description}</div>
+          <div className="update-date">Reportado por: {update.createdBy}</div>
+        </li>
+        ))}
+      </ol>
+      </div>
   )
 }
